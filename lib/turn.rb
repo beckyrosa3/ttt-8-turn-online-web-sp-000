588d92cc81ecc71dc28696d3c80e69f79127a0b7
Becky Rosa <becky_m_rosa@yahoo.com>
2021-01-02 02:54:17 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,7 +23,7 @@ elsif board[index] == " " || "" || nil
 end
 
 def valid_move?(board, index)
-  index = user_input.to_i-1
+  board = index.to_i-1
   if index.between?(0,8) && !position_taken?(board, index)
     true
   else
